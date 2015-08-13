8e0abeaa15ed342677368f4bf43b33c0dc502af6
Nakul Pathak <nakul.pathak@shopify.com>
2015-08-13 15:53:19 +0000

    Add author association. inherit from parent appcontroller

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,5 +1,18 @@
 module Blorgh
   class Article < ActiveRecord::Base
+
+    attr_accessor :author_name
+    belongs_to :author, class_name: "User"
+
+    before_save :set_author
+
     has_many :comments
+
+    private
+
+    def set_author
+      self.author = User.find_or_create_by(name: author_name)
+    end
+
   end
 end
