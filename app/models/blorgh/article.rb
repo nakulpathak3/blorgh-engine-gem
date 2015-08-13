9b93a2593487d66d4dff12a773232168d03a8477
Nakul Pathak <nakul.pathak@shopify.com>
2015-08-13 17:08:08 +0000

    Make blorgh pick up class from unicorn initializer

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -2,7 +2,7 @@ module Blorgh
   class Article < ActiveRecord::Base
 
     attr_accessor :author_name
-    belongs_to :author, class_name: "User"
+    belongs_to :author, class_name: Blorgh.author_class.to_s
 
     before_save :set_author
 
@@ -11,7 +11,7 @@ module Blorgh
     private
 
     def set_author
-      self.author = User.find_or_create_by(name: author_name)
+      self.author = Blorgh.author_class.find_or_create_by(name: author_name)
     end
 
   end
